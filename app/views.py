65474a9f104e7c50f4dcb5602bc677be7f756bb7
saltypie <louisrangi1@gmail.com>
2021-11-12 14:53:13 +0300

    add user API view class and Proifle user API

--- app/views.py
+++ app/views.py
@@ -6,5 +6,18 @@ import pandas as pd
 from sklearn import preprocessing                              
 from sklearn.neighbors import KNeighborsClassifier                       
 import numpy as np 
-
+from app.serializer import *
+from django.contrib.auth.models import User
 # Create your views here.
+
+class ProfileApi(APIView):
+    def get(self, request, format=None):
+        profs = Profile.objects.all()
+        serializers = ProfileSerializer(profs, many=True)
+        return Response(serializers.data)   
+
+class UserApi(APIView):
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializers = ProfileSerializer(users, many=True)
+        return Response(serializers.data)       
