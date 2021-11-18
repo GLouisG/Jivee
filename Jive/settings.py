b8d16d9a43a042e62d02e4c211e638515b9af34b
saltypie <louisrangi1@gmail.com>
2021-11-18 10:38:08 +0300

    finishing touches on connecting to api adding cors

--- Jive/settings.py
+++ Jive/settings.py
@@ -183,7 +183,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
  
 #Cors
 CORS_ALLOWED_ORIGINS = [
-   'http://localhost:4200/home',
+   'http://localhost:4200/home/',
 ]
 django_heroku.settings(locals())
 
