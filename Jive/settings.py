24dc1541255f046ef25f74329d54675b441ef809
saltypie <louisrangi1@gmail.com>
2021-11-18 12:15:28 +0300

    finishing touches on connecting to api adding cors

--- Jive/settings.py
+++ Jive/settings.py
@@ -60,6 +60,8 @@ MIDDLEWARE = [
    'django.contrib.auth.middleware.AuthenticationMiddleware',
    'django.contrib.messages.middleware.MessageMiddleware',
    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+       'corsheaders.middleware.CorsMiddleware',
+
 ]
  
 ROOT_URLCONF = 'Jive.urls'
